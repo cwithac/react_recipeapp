1760616870290f9fd85a782b5702df12d7c31778
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-31 21:24:25 -0500

    Recipe List js component and CSS

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import Recipe from './Recipe';
 import Navbar from './Navbar';
+import RecipeList from './RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar/>
-        <Recipe
-        title="pasta"
-        ingredients={['flour', 'water']}
-        instructions="Mix ingredients"
-        img='pasta.jpg'
-        />
+        <Navbar/>
+        <RecipeList />
       </div>
     );
   }
