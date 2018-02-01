7db9c44687eed5166a19f881329a3bf9a80c8240
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-31 20:23:47 -0500

    Create and import Recipe component

---
+++ src/Recipe.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Recipe.css';
+
+class Recipe extends Component {
+  render() {
+    return (
+      <div>Recipe</div>
+    );
+  }
+}
+
+export default Recipe;
