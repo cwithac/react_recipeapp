24e466fb33b43c775445187a35cc0500e744b44e
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-06 13:38:34 -0500

    Recipe input return

--- src/RecipeInput.js
+++ src/RecipeInput.js
@@ -31,6 +31,75 @@ class RecipeInput extends Component {
               onChange={() => {}} />
         </div>
       ));
+
+        return (
+        <div className="recipe-form-container">
+          <form className='recipe-form' onSubmit={this.handleSubmit}>
+            <button
+              type="button"
+              className="close-button"
+              onClick={onClose}
+            >
+              X
+            </button>
+            <div className='recipe-form-line'>
+              <label htmlFor='recipe-title-input'>Title</label>
+              <input
+                id='recipe-title-input'
+                key='title'
+                name='title'
+                type='text'
+                value={title}
+                size={42}
+                autoComplete="off"
+                onChange={this.handleChange}/>
+            </div>
+            <label
+              htmlFor='recipe-instructions-input'
+              style={{marginTop: '5px'}}
+            >
+              Instructions
+            </label>
+            <textarea
+              key='instructions'
+              id='recipe-instructions-input'
+              type='Instructions'
+              name='instructions'
+              rows='8'
+              cols='50'
+              autoComplete='off'
+              value={instructions}
+              onChange={this.handleChange}/>
+            {inputs}
+            <button
+              type="button"
+              onClick={this.handleNewIngredient}
+              className="buttons"
+            >
+              +
+            </button>
+            <div className='recipe-form-line'>
+              <label htmlFor='recipe-img-input'>Image Url</label>
+              <input
+                id='recipe-img-input'
+                type='text'
+                placeholder=''
+                name='img'
+                value={img}
+                size={36}
+                autoComplete='off'
+                onChange={this.handleChange} />
+            </div>
+            <button
+              type="submit"
+              className="buttons"
+              style={{alignSelf: 'flex-end', marginRight: 0}}
+            >
+              SAVE
+            </button>
+          </form>
+        </div>
+      )
     }
 }
 
