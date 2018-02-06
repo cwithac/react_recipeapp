6f1234bbc4fc6c8194958488b034b435a19517a6
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-06 13:44:00 -0500

    Handle Change Functions

--- src/RecipeInput.js
+++ src/RecipeInput.js
@@ -12,7 +12,41 @@ class RecipeInput extends Component {
         ingredients: [''],
         img: ''
       };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleNewIngredient = this.handleNewIngredient.bind(this);
+      this.handleChangeIng = this.handleChangeIng.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(e) {
+      this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleNewIngredient(e) {
+      const {ingredients} = this.state;
+      this.setState({ingredients: [...ingredients, '']});
+    }
+
+    handleChangeIng(e) {
+      const index = Number(e.target.name.split('-')[1]);
+      const ingredients = this.state.ingredients.map((ing, i) => (
+        i === index ? e.target.value : ing
+      ));
+      this.setState({ingredients});
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      this.props.onSave({...this.state});
+      this.setState({
+        title: '',
+        instructions: '',
+        ingredients: [''],
+        img: ''
+      })
+    }
+
     render() {
       const {title, instructions, img, ingredients} = this.state;
       let inputs = ingredients.map((ing, i) => (
